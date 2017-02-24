6de4f2891b13b8676395c84e08831abc445852dd
jhansonpk <jhansonpk@hotmail.com>
2017-02-24 16:15:14 -0300

    init

---
+++ controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+namespace controllers;
+
+class Controller
+{
+	
+	public function __construct()  
+    {
+
+    } 
+	
+	public function run()
+	{
+
+	}
+}
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php 
+	include_once("../controllers/Controller.php");
+
+	$controller = new \controllers\Controller();
+	$controller->run();
