6de7b6cf3f3aac30ed6614e2328c4d18465f0018
jhansonpk <jhansonpk@hotmail.com>
2017-02-28 03:53:52 -0300

    no message

--- Model.php
+++ Model.php
@@ -10,7 +10,6 @@ class Model
     {
         $data = [];
 
-        // hard code para popular o model Book
         $data[1] = new Book(1, 'Império De Tempestades - Vol.5 - Tomo 2 - Série Trono De Vidro', 'Sarah J. Maas', 'A história de Aelin Galathynius, sempre repleta de ação e intrigas, continua nesta segunda parte do quinto livro da série, Império de tempestades Aelin Galathyius sobreviveu a prisão, à perda de amigos e amores, às traições. Agora deve vencer seu maior medo para salvar o mundo. Com a vida e poder jurados ao povo que está determinada a salvar, a antiga assassina, conhecida como Celaena Sardothien, colocará a própria segurança em risco para proteger os seus. Mais que nunca, Aelin precisa de Rowan, de Dorian e de todos os aliados para conseguir descobrir a localização da relíquia sagrada capaz de banir de seu mundo a ameaça valg e os horrores libertados em Morath. Chegou a hora de levantar os exércitos de Erilea. De cobrar velhas dívidas... É hora de marchar contra o mais supremo dos males. E confiar na pureza de seu coração para trazer a luz.');
         $data[2] = new Book(2, 'Harry Potter E A Câmara Secreta - Edição Ilustrada', 'J. K. Rowling', 'Depois de Harry Potter e a Pedra Filosofal, é a vez de Harry Potter e a Câmara Secreta chegar às prateleiras em edição ilustrada e capa dura. Cenas de tirar o fôlego, momentos tensos da trama e personagens inesquecíveis esperam os leitores nesta edição mágica que traz o texto integral de J.K. Rowling e desenhos do premiado Jim Kay. Com tinta, papel e pixels, o ilustrador britânico cria um mundo encantado como nunca antes visto para acompanhar o segundo ano de Harry Potter na Escola de Magia de Hogwarts.');
         $data[3] = new Book(3, 'Signos Da Marca: Expressividade E Sensorialidade', 'Clotilde Perez', 'Compre o livro Signos da Marca: Expressividade e Sensorialidade em até 6x sem juros. A Cia. dos Livros oferece mais de 200 mil livros com os melhores preços. Confira! O livro Signos da Marca: Expressividade e Sensorialidade, da autora Clotilde Perez, está em sua 2ª edição publicada pela editora Cengage Learning Nacional. Esta edição traz uma cuidadosa atualização e adensamento dos conceitos centrais da comunicação e gestão das marcas, no contexto da sociedade contemporânea, e aponta caminhos possíveis para os gestores das marcas no sentido de potencializarem sua força comunicacional e, portanto, a capacidade de construírem vínculos com os consumidores-alvo. A obra é ideal tanto para estudantes como para profissionais da área de Gestão Estratégica de Marca, pois apresenta análises semióticas que auxiliam na melhoria de resultados de pesquisa de mercado. Compre o livro Signos da Marca: Expressividade e Sensorialidade no site da Cia. dos Livros.');
@@ -20,8 +19,7 @@ class Model
 
     public function getBook($id)
     {
-        // we use the previous function to get all the books and then we return the requested one.
-        // in a real life scenario this will be done through a db select command
+
         $allBooks = $this->getBookList();
         if(isset($allBooks[$id]))
             return $allBooks[$id];
