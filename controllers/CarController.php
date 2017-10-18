3d5ae2f030a500919e9151d407b902a73f287556
André Martins <jhansonpk@hotmail.com>
2017-10-18 17:46:50 -0200

    init car controller

---
+++ controllers/CarController.php
@@ -0,0 +1,35 @@
+<?php
+namespace mvc\controllers;
+
+use mvc\Model;
+
+/**
+ * Class BookController
+ * @package mvc\controllers
+ */
+class CarController
+{
+    public $model;
+
+    public function __construct()
+    {
+        $this->model = new Model();
+
+    }
+
+
+    public function actionIndex()
+    {
+        $books = $this->model->getBookList();
+        include $this->viewFile;
+    }
+
+    /**
+     * @param $id
+     */
+    public function actionView($id)
+    {
+        $book = $this->model->getBook($id);
+        include $this->viewFile;
+    }
+}
